cba59f52bbeebcee301cc77dd21ac9cd68a4783f
BIG-LAZY <1009197367@qq.com>
2024-07-10 22:56:13 +0800

    solve file name with space

--- traverse_directory.sh
+++ traverse_directory.sh
@@ -3,6 +3,6 @@
 # 输出 JSON 数组的开头
 echo "[" > directory_info.json
 # 遍历根目录
-find ./ -name "*.md" -exec ls -l --time-style=+"%Y-%m-%d %H:%M:%S" {} + | sort -r -k 6,7 | awk '{print "{\"filepath\":\""$8"\",\"size\":\""$5"\",\"last_modified\":\""$6" "$7"\"},"}' | sed '$ s/,$//' >> directory_info.json
+find ./ -name "*.md" -exec ls -l --time-style=+"%Y-%m-%d %H:%M:%S" {} + | sort -r -k 6,7 | awk '{print "{\"filepath\":\""substr($0, index($0,$8))"\",\"size\":\""$5"\",\"last_modified\":\""$6" "$7"\"},"}' | sed '$ s/,$//' >> directory_info.json
 # 输出 JSON 数组的结尾
 echo "]" >> directory_info.json
